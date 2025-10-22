9dd58a1533ce7b24d67cd5d65145d291b896a0fe
Juan Manuel Piñero Sánchez <jpinero@accedia.es>
2025-10-22 18:35:23 +0200

    contador 1 a 10

---
+++ EjerciciosJava/001/proyectos/contador1a10/src/main/java/com/accedia/tarde/Contador1a100.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.accedia.tarde;
+
+/**
+ *
+ * @author Tarde Juanma
+ */
+public class Contador1a100 {
+    public static void main(String[] args) {
+        for (int i=1; i<=10; i++) {
+            System.out.printf("Numero: %d\n",i);
+        }
+    }
+}
