6f386a519484cd4920ffb0000aaf526d383ab7ff
Juan Manuel Piñero Sánchez <jpinero@accedia.es>
2025-10-23 17:40:23 +0200

    cuenta atras

---
+++ EjerciciosJava/001/proyectos/CuentaAtras/src/main/java/com/accedia/tarde/cuentaatras/CuentaAtras.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.accedia.tarde.cuentaatras;
+
+/**
+ *
+ * @author Tarde Juanma
+ */
+public class CuentaAtras {
+
+    public static void main(String[] args) {
+        int numero;
+        
+        numero=10;
+        while(numero >= 1) {
+            System.out.println(numero);
+            numero = numero - 1;
+        }
+    }
+}
