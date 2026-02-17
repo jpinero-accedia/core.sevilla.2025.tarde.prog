aa5ed38a8bc8ed5f33e88b1fde3529f552b945ce
Juan Manuel Piñero Sánchez <jpinero@accedia.es>
2026-02-17 16:14:02 +0100

    WIP genericos

---
+++ TeoriaJava/genericos/genericos01/src/main/java/com/accedia/tarde/genericos01/Genericos01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.accedia.tarde.genericos01;
+
+/**
+ *
+ * @author Tarde Juanma
+ */
+public class Genericos01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
